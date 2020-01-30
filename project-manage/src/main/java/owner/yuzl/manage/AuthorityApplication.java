42581277b1f54a5b16e9895c6fad7fde9852a185
yzl-c <1120779716@qq.com>
2020-01-30 17:03:09 +0800

    【数据库里表名改小写】

--- project-manage/src/main/java/owner/yuzl/manage/AuthorityApplication.java
+++ project-manage/src/main/java/owner/yuzl/manage/AuthorityApplication.java
@@ -3,9 +3,9 @@ package owner.yuzl.manage;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @MapperScan("owner.yuzl.manage.mapper")
 public class AuthorityApplication {
 
